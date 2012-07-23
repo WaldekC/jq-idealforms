6f47c78491f05a8a68b65207d7b7996c40c6fc03
elclanrs <elclanrs@gmail.com>
2012-07-23 06:17:37 -0400

    u

--- js/filters.js
+++ js/filters.js
@@ -30,207 +30,216 @@ $.idealforms.errors = {
 }
 
 /**
- * Built-in filters
+ * Get all default filters
+ * @returns object
  */
-$.idealforms.filters = {
 
-  required: {
-    error: $.idealforms.errors.required
-  },
+$.idealforms.filters = {} // Init global filters
 
-  number: {
-    regex: /\d+/,
-    error: $.idealforms.errors.number
-  },
+var getFilters = function () {
 
-  digits: {
-    regex: /^\d+$/,
-    error: $.idealforms.errors.digits
-  },
+  var filters = {
 
-  name: {
-    regex: /^[A-Za-z]{3,}$/,
-    error: $.idealforms.errors.name
-  },
+    required: {
+      error: $.idealforms.errors.required
+    },
 
-  username: {
-    regex: /^[a-z](?=[\w.]{3,31}$)\w*\.?\w*$/i,
-    error: $.idealforms.errors.username
-  },
+    number: {
+      regex: /\d+/,
+      error: $.idealforms.errors.number
+    },
 
-  pass: {
-    regex: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
-    error: $.idealforms.errors.pass
-  },
+    digits: {
+      regex: /^\d+$/,
+      error: $.idealforms.errors.digits
+    },
 
-  strongpass: {
-    regex: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-    error: $.idealforms.errors.strongpass
-  },
+    name: {
+      regex: /^[A-Za-z]{3,}$/,
+      error: $.idealforms.errors.name
+    },
 
-  email: {
-    regex: /[^@]+@[^@]/,
-    error: $.idealforms.errors.email
-  },
+    username: {
+      regex: /^[a-z](?=[\w.]{3,31}$)\w*\.?\w*$/i,
+      error: $.idealforms.errors.username
+    },
 
-  phone: {
-    regex: /^[2-9]\d{2}-\d{3}-\d{4}$/,
-    error: $.idealforms.errors.phone
-  },
+    pass: {
+      regex: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+      error: $.idealforms.errors.pass
+    },
 
-  zip: {
-    regex: /^\d{5}$|^\d{5}-\d{4}$/,
-    error: $.idealforms.errors.zip
-  },
+    strongpass: {
+      regex: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+      error: $.idealforms.errors.strongpass
+    },
 
-  url: {
-    regex: /^(?:(ftp|http|https):\/\/)?(?:[\w\-]+\.)+[a-z]{3,6}$/i,
-    error: $.idealforms.errors.url
-  },
+    email: {
+      regex: /[^@]+@[^@]/,
+      error: $.idealforms.errors.email
+    },
 
-  min: {
-    regex: function (input, value) {
-      var $input = input.input,
-          min = input.userOptions.data.min,
-          isRadioCheck = $input.is('[type="checkbox"], [type="radio"]')
-      if (isRadioCheck) {
-        this.error = $.idealforms.errors.minOption.replace('{0}', min)
-        return $input.filter(':checked').length >= min
-      }
-      this.error = $.idealforms.errors.minChar.replace('{0}', min)
-      return value.length >= min
-    }
-  },
+    phone: {
+      regex: /^[2-9]\d{2}-\d{3}-\d{4}$/,
+      error: $.idealforms.errors.phone
+    },
 
-  max: {
-    regex: function (input, value) {
-      var $input = input.input,
-          max = input.userOptions.data.max,
-          isRadioCheck = $input.is('[type="checkbox"], [type="radio"]')
-      if (isRadioCheck) {
-        this.error = $.idealforms.errors.maxOption.replace('{0}', max)
-        return $input.filter(':checked').length <= max
-      }
-      this.error = $.idealforms.errors.maxChar.replace('{0}', max)
-      return value.length <= max
-    }
-  },
+    zip: {
+      regex: /^\d{5}$|^\d{5}-\d{4}$/,
+      error: $.idealforms.errors.zip
+    },
 
-  range: {
-    regex: function (input, value) {
-      var range = input.userOptions.data.range,
-          val = +value
-      this.error = $.idealforms.errors.range
-        .replace('{0}', range[0])
-        .replace('{1}', range[1])
-      return val >= range[0] && val <= range[1]
-    }
-  },
+    url: {
+      regex: /^(?:(ftp|http|https):\/\/)?(?:[\w\-]+\.)+[a-z]{3,6}$/i,
+      error: $.idealforms.errors.url
+    },
 
-  date: {
-    regex: function (input, value) {
-      var
-
-      userFormat =
-        input.userOptions.data && input.userOptions.data.date
-          ? input.userOptions.data.date
-          : 'mm/dd/yyyy', // default format
-
-      delimiter = /[^mdy]/.exec(userFormat)[0],
-      theFormat = userFormat.split(delimiter),
-      theDate = value.split(delimiter),
-
-      isDate = function (date, format) {
-        var m, d, y
-        for (var i = 0, len = format.length; i < len; i++) {
-          if (/m/.test(format[i])) m = date[i]
-          if (/d/.test(format[i])) d = date[i]
-          if (/y/.test(format[i])) y = date[i]
+    min: {
+      regex: function (input, value) {
+        var $input = input.input,
+            min = input.userOptions.data.min,
+            isRadioCheck = $input.is('[type="checkbox"], [type="radio"]')
+        if (isRadioCheck) {
+          this.error = $.idealforms.errors.minOption.replace('{0}', min)
+          return $input.filter(':checked').length >= min
         }
-        return (
-          m > 0 && m < 13 &&
-          y && y.length === 4 &&
-          d > 0 && d <= (new Date(y, m, 0)).getDate()
-        )
+        this.error = $.idealforms.errors.minChar.replace('{0}', min)
+        return value.length >= min
       }
+    },
 
-      this.error = $.idealforms.errors.date.replace('{0}', userFormat)
-
-      return isDate(theDate, theFormat)
-    }
-  },
-
-  dob: {
-    regex: function (input, value) {
-      var
-
-      userFormat =
-        input.userOptions.data && input.userOptions.data.dob
-          ? input.userOptions.data.dob
-          : 'mm/dd/yyyy', // default format
-
-      // Simulate a date input
-      dateInput = {
-        input: input.input,
-        userOptions: {
-          data: { date: userFormat }
+    max: {
+      regex: function (input, value) {
+        var $input = input.input,
+            max = input.userOptions.data.max,
+            isRadioCheck = $input.is('[type="checkbox"], [type="radio"]')
+        if (isRadioCheck) {
+          this.error = $.idealforms.errors.maxOption.replace('{0}', max)
+          return $input.filter(':checked').length <= max
         }
-      },
+        this.error = $.idealforms.errors.maxChar.replace('{0}', max)
+        return value.length <= max
+      }
+    },
 
-      // Use internal date filter to validate the date
-      isDate = filters.date.regex(dateInput, value),
+    range: {
+      regex: function (input, value) {
+        var range = input.userOptions.data.range,
+            val = +value
+        this.error = $.idealforms.errors.range
+          .replace('{0}', range[0])
+          .replace('{1}', range[1])
+        return val >= range[0] && val <= range[1]
+      }
+    },
 
-      // DOB
-      theYear = /\d{4}/.exec(value),
-      maxYear = new Date().getFullYear(), // Current year
-      minYear = maxYear - 100
+    date: {
+      regex: function (input, value) {
+        var
 
-      this.error = $.idealforms.errors.dob
+        userFormat =
+          input.userOptions.data && input.userOptions.data.date
+            ? input.userOptions.data.date
+            : 'mm/dd/yyyy', // default format
 
-      return isDate &&
-        theYear >= minYear &&
-        theYear <= maxYear
-    }
-  },
+        delimiter = /[^mdy]/.exec(userFormat)[0],
+        theFormat = userFormat.split(delimiter),
+        theDate = value.split(delimiter),
 
-  exclude: {
-    regex: function (input, value) {
-      var i = input.input
-      if (i.is('[type="checkbox"], [type="radio"], select'))
-        this.error = $.idealforms.errors.excludeOption.replace('{0}', value)
-      else
-        this.error = $.idealforms.errors.exclude.replace('{0}', value)
-      return !~$.inArray(value, input.userOptions.data.exclude)
-    }
-  },
+        isDate = function (date, format) {
+          var m, d, y
+          for (var i = 0, len = format.length; i < len; i++) {
+            if (/m/.test(format[i])) m = date[i]
+            if (/d/.test(format[i])) d = date[i]
+            if (/y/.test(format[i])) y = date[i]
+          }
+          return (
+            m > 0 && m < 13 &&
+            y && y.length === 4 &&
+            d > 0 && d <= (new Date(y, m, 0)).getDate()
+          )
+        }
 
-  equalto: {
-    regex: function (input, value) {
-      var $equals = $(input.userOptions.data.equalto),
-          $input = input.input,
-          name = $equals.attr('name') || $equals.attr('id'),
-          isValid =
-            $equals
-              .parents('.ideal-field')
-              .filter(function(){ return $(this).data('isValid') === true })
-              .length
-      if (!isValid) return false
-      this.error = $.idealforms.errors.equalto.replace('{0}', name)
-      return $input.val() === $equals.val()
-    }
-  },
+        this.error = $.idealforms.errors.date.replace('{0}', userFormat)
 
-  extension: {
-    regex: function (input, value) {
-      var files = input.input[0].files || [{ name: value }],
-          extensions = input.userOptions.data.extension,
-          re = new RegExp('\\.'+ extensions.join('|') +'$', 'i'),
-          valid = false
-      for (var i = 0, len = files.length; i < len; i++)
-        valid = re.test(files[i].name) ? true : false
-      this.error = $.idealforms.errors.extension.replace('{0}', extensions.join('", "'))
-      return valid
+        return isDate(theDate, theFormat)
+      }
+    },
+
+    dob: {
+      regex: function (input, value) {
+        var
+
+        userFormat =
+          input.userOptions.data && input.userOptions.data.dob
+            ? input.userOptions.data.dob
+            : 'mm/dd/yyyy', // default format
+
+        // Simulate a date input
+        dateInput = {
+          input: input.input,
+          userOptions: {
+            data: { date: userFormat }
+          }
+        },
+
+        // Use internal date filter to validate the date
+        isDate = filters.date.regex(dateInput, value),
+
+        // DOB
+        theYear = /\d{4}/.exec(value),
+        maxYear = new Date().getFullYear(), // Current year
+        minYear = maxYear - 100
+
+        this.error = $.idealforms.errors.dob
+
+        return isDate &&
+          theYear >= minYear &&
+          theYear <= maxYear
+      }
+    },
+
+    exclude: {
+      regex: function (input, value) {
+        var i = input.input
+        if (i.is('[type="checkbox"], [type="radio"], select'))
+          this.error = $.idealforms.errors.excludeOption.replace('{0}', value)
+        else
+          this.error = $.idealforms.errors.exclude.replace('{0}', value)
+        return !~$.inArray(value, input.userOptions.data.exclude)
+      }
+    },
+
+    equalto: {
+      regex: function (input, value) {
+        var $equals = $(input.userOptions.data.equalto),
+            $input = input.input,
+            name = $equals.attr('name') || $equals.attr('id'),
+            isValid =
+              $equals
+                .parents('.ideal-field')
+                .filter(function(){ return $(this).data('isValid') === true })
+                .length
+        if (!isValid) return false
+        this.error = $.idealforms.errors.equalto.replace('{0}', name)
+        return $input.val() === $equals.val()
+      }
+    },
+
+    extension: {
+      regex: function (input, value) {
+        var files = input.input[0].files || [{ name: value }],
+            extensions = input.userOptions.data.extension,
+            re = new RegExp('\\.'+ extensions.join('|') +'$', 'i'),
+            valid = false
+        for (var i = 0, len = files.length; i < len; i++)
+          valid = re.test(files[i].name) ? true : false
+        this.error = $.idealforms.errors.extension.replace('{0}', extensions.join('", "'))
+        return valid
+      }
     }
   }
-}
 
+  return filters
+
+}
